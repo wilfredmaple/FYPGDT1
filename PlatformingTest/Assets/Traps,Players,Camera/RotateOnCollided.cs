6f21f919c7e9e2b25a5f02494376a954ca72804d
LilF <wilfredmaple@gmail.com>
2018-09-06 17:42:38 +0800

    wilwil scene, gonna do the maze tmr?

---
+++ PlatformingTest/Assets/Traps,Players,Camera/RotateOnCollided.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnCollided : MonoBehaviour {
+    private Rigidbody RigidRef;
+    // Use this for initialization
+    void Start () {
+        RigidRef = GetComponent<Rigidbody>();
+        RigidRef.constraints = RigidbodyConstraints.FreezeAll;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("HAHA SURPRISE MOFO");
+            transform.Rotate(0, 0, 90);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("HAHA SURPRISE MOFO");
+            
+        }
+    }
+}
